48c204bb9eca92a961bc733704ffa1221f192c80
Amin Tuarshev <atuarshev@gmail.com>
2023-07-01 19:42:02 +0300

    Удалил код краш теста

--- backend/app.js
+++ backend/app.js
@@ -28,12 +28,6 @@ app.use(helmet());
 
 app.use(requestLogger);
 
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
-
 app.use(router);
 app.use(errorLogger);
 
